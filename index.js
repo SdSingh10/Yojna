e582000c2e88185d80356a11b614ee1892fe3407
Isha-Nanda08 <143190568+Isha-Nanda08@users.noreply.github.com>
2024-02-24 22:16:28 +0530

    add files

---
+++ index.js
@@ -0,0 +1,56 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+
+const port=3300;
+const app= express();
+let username="user";
+let password="user@";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/login",(req,res)=>{
+    const checkbox=req.body.check;
+    password=req.body.pass;
+    username=req.body.aadhar;
+    
+    res.redirect("/");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("index.ejs",{userName:username});
+})
+app.get("/logout",(req,res)=>{
+    username="user";
+    res.redirect("/login");
+})
+app.get("/profile",(req,res)=>{
+    res.render("profile.ejs");
+})
+app.get("/notifications",(req,res)=>{
+    res.render("notification.ejs");
+})
+app.get("/feedback",(req,res)=>{
+    res.render("feedback.ejs");
+})
+
+app.post("/feedback",(req,res)=>{
+    
+    res.redirect("/");
+})
+
+
+
+app.get("/",(req,res)=>{
+    res.render("yojna.ejs");
+})
+app.get("/reset",(req,res)=>{
+    res.render("setpass.ejs");
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`your running port is : ${port}`);
+})
